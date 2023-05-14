6ce7c788dc7e3dc2f51af1f49bfa3bee8e75b1b9
Akimosabe <he7seven@gmail.com>
2023-05-14 16:59:14 +0500

    add all elements

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -19,9 +19,31 @@ public class Parser {
 
         Element table = page.select("table[class=weather]").first();
         //System.out.println(table);
-        Elements names = table.select("td[class=day]");
-        String date  ;
-        System.out.println(names);
+        //Elements names = table.select("td[class=day]");
+
+        Element dateElement = table.select("td.day span").last();
+        String date = dateElement.text() ;
+        //Вытаскиваем дату текстом
+
+        Elements timeElement = table.select("span[class=c1]");
+        String time = timeElement.text() ;
+        //Вытаскиваем время
+
+        Elements temperatureElement = table.select("span[class=ht]");
+        String temperature = temperatureElement.text() ;
+        //Вытаскиваем температуру
+
+        Elements windElement = table.select("span[class=wd]");
+        String wind = windElement.text();
+        Elements windspeedElement = table.select("span[class=ws]");
+        String windspeed = windspeedElement.text();
+        //Вытаскиваем ветер
+
+        Elements tempTextElement = table.select("div");
+        String tempText = tempTextElement.text();
+        //Вытаскиваем описание погоды
+
+        System.out.println(date+time+temperature+wind+windspeed+tempText);
 
 }
 }
