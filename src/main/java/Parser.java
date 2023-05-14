6fa193dc404393982d7645a3ba28959164908916
Akimosabe <he7seven@gmail.com>
2023-05-14 15:35:39 +0500

    start

---
+++ src/main/java/Parser.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class Parser {
+    private static Document getPage() throws IOException{
+         String url = "https://ekaterinburg.nuipogoda.ru/погода-на-сегодня";
+         Document page = Jsoup.parse(new URL(url), 3000);
+         return page;
+    }
+
+    public static void main(String[] args) throws IOException{
+        System.out.println(getPage());
+}
+}
